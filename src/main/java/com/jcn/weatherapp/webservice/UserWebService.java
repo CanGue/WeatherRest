8d6143c80b0a10bf1f0cb198edd4ffacb646f40d
Can Güler <GUC@isoad.isogmbh.de>
2017-10-06 18:56:53 +0200

    added Rest Path to get current weather from Users default city

--- src/main/java/com/jcn/weatherapp/webservice/UserWebService.java
+++ src/main/java/com/jcn/weatherapp/webservice/UserWebService.java
@@ -1,5 +1,6 @@
 package com.jcn.weatherapp.webservice;
 
+import java.util.Optional;
 
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
@@ -9,9 +10,8 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Response;
 import javax.ws.rs.core.MediaType;
-
+import javax.ws.rs.core.Response;
 
 import com.jcn.weatherapp.Interceptor.RestLogger;
 import com.jcn.weatherapp.dao.UserDAO;
@@ -24,25 +24,26 @@ public class UserWebService {
 
 	@EJB
 	private UserDAO userDAO;
+	
+	@EJB
+	private CurrentWeatherService currentWeatherService;
 
 	@GET
-    @Produces(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
 	public Response getAllUsers() {
 
 		return userDAO.findAllUsers().map(v -> Response.ok().entity(v).build()).orElse(Response.noContent().build());
 	}
-	
-	
+
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("{userid}")
-	public Response getUser(@PathParam("userid") Long userid)
-	{
-		return userDAO.read(new User(),userid).map(v -> Response.ok().entity(v).build()).orElse(Response.noContent().build());
+	public Response getUser(@PathParam("userid") Long userid) {
+		return userDAO.read(new User(), userid).map(v -> Response.ok().entity(v).build())
+				.orElse(Response.noContent().build());
 	}
-	
-	
+
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
@@ -52,6 +53,17 @@ public class UserWebService {
 		User user = new User();
 		user.setId(userid);
 		return userDAO.findUsersDefaultCity(user).map(v -> Response.ok().entity(v).build()).orElse(Response.noContent().build());
+	}
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("{userid}/defaultCity/currentWeather")
+	public Response getCurrentWeatherOfUsersDefaultlCity(@PathParam("userid") Long userid){
+		User user = new User();
+		user.setId(userid);
+		Optional<String> findUsersDefaultCity = userDAO.findUsersDefaultCity(user);
+		return currentWeatherService.getCurrenWeatherByCity(findUsersDefaultCity.get());
 	}
 
 }
